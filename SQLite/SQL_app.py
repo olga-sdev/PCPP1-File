68b967fd645140a671d1b547c66d83d28bc016ef
olga-sdev <olga.github@airpost.net>
2025-04-29 18:37:06 +0300

    Update SQL_app.py

--- SQLite/SQL_app.py
+++ SQLite/SQL_app.py
@@ -27,6 +27,7 @@ class Application:
         self.c.execute("INSERT INTO job_application (full_name, email, summary)"
                        "VALUES (:full_name, :email, :summary)", data)
         self.conn.commit()
+        self.conn.close()
 
     def read_job_app(self):
         name = input('Enter name of candidate: ')
@@ -52,3 +53,25 @@ if __name__ == "__main__":
         app.read_job_app()
     elif choice == '3':
         app.read_all_data()
+
+
+"""
+Manipulation with DB: 1-add new data; 2-read data by name; 3-read all data.
+ Select option: 3
+(1, 'John Doe', 'john@mail.com', 'Python developer with 10 years of experience: Flask, Django, FastAPI, Tkinter, Kivy')
+(2, 'Anna Smith', 'anna@mail.com', 'QA Engineer: manual and automation testing with Python')
+(3, 'Viktor Micle', 'viktor@mail.com', 'DevOps: AWS, GCP, Aure, CICD, Jenkins')
+(4, 'Adam', 'adam@mail.com', 'Scrum Master')
+(5, 'George', 'ge@mail.com', 'PO')
+
+Manipulation with DB: 1-add new data; 2-read data by name; 3-read all data.
+ Select option: 1
+Enter full name: Dan
+Enter email: dan@mail.com
+Enter brief summary: PM
+
+Manipulation with DB: 1-add new data; 2-read data by name; 3-read all data.
+ Select option: 2
+Enter name of candidate: Dan
+(6, 'Dan', 'dan@mail.com', 'PM')
+"""
