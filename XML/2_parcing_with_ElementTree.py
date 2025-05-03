e3e19153533e0636709024539d0e11b98342c19a
olga-sdev <olga.github@airpost.net>
2025-05-03 19:30:45 +0300

    Update 2_parcing_with_ElementTree.py

--- XML/2_parcing_with_ElementTree.py
+++ XML/2_parcing_with_ElementTree.py
@@ -29,6 +29,24 @@ for course in root:
           '\nCategory of the course:', course[0].text,
           '\nYear of the course:', course[1].text)
 
+print('=================')
+
+
+tree = ET.parse('courses.xml')
+root = tree.getroot()
+
+# Example: findall method searches the child elements at the first nesting level
+for course in root.findall('course'):
+    print('Findall method output:', course.get('title'))
+
+# Example: find method returns the first child element containing the specified tag or matching XPath expression
+print('Find method output:', root.find('course').get('title'))
+
+# Example: iter method returns all elements
+for category in root.iter('category'):
+    print('Iter category method output:', category.text)
+
+
 """
 Example with ElementTree and parse method
 
@@ -40,4 +58,10 @@ Example with fromstring method
 Title of the course: Networking 
 Category of the course: Information Technology 
 Year of the course: 2024
+=================
+Findall method output: Agile methodolody
+Findall method output: Networking
+Find method output: Agile methodolody
+Iter category method output: Project management
+Iter category method output: Information Technology
 """
